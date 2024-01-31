c93d066a743b8e6b460eeb88beb90d8f9aa8232e
Marcelo Augusto <mrcelo014@gmail.com>
2024-01-31 10:20:43 -0300

    Handling GET Request

---
+++ router-handlers/src/app/comments/data.ts
@@ -0,0 +1,6 @@
+export const comments = [
+  { id: 1, texto: 'Ótimo artigo, muito esclarecedor!' },
+  { id: 2, texto: 'Gostei bastante do conteúdo, parabéns!' },
+  { id: 3, texto: 'Precisa de mais detalhes, poderia ser mais explicativo.' },
+  { id: 4, texto: 'Interessante, mas acho que falta profundidade.' }
+]
