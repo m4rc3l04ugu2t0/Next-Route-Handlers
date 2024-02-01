b98abeeae7a186eaab091110e1a4606312c65646
Marcelo Augusto <mrcelo014@gmail.com>
2024-01-31 22:49:00 -0300

    Headers in Route Handlers

--- router-handlers/src/app/users/api/route.ts
+++ router-handlers/src/app/users/api/route.ts
@@ -1,3 +1,16 @@
-export default async function dataUsers() {
-  return await new Response('Data users')
+import { type NextRequest } from 'next/server'
+import { headers } from 'next/headers'
+
+export async function GET(request: NextRequest) {
+  const headersRequest = new Headers(request.headers)
+  const headerListb = headers()
+
+  console.log(headersRequest.get('Authorization'))
+  console.log(headerListb.get('Authorization'))
+
+  return new Response('<h1>Data users</h1>', {
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  })
 }
