ee6772a68ad02889c75268c8dce57375ef31c681
Marcelo Augusto <mrcelo014@gmail.com>
2024-02-01 09:06:37 -0300

    Cookies in Route Handlers

--- router-handlers/src/app/users/api/route.ts
+++ router-handlers/src/app/users/api/route.ts
@@ -1,16 +1,22 @@
 import { type NextRequest } from 'next/server'
-import { headers } from 'next/headers'
+import { cookies, headers } from 'next/headers'
 
 export async function GET(request: NextRequest) {
   const headersRequest = new Headers(request.headers)
   const headerListb = headers()
+  const theme = request.cookies.get('theme')
+
+  cookies().set('themeLight', 'light')
 
   console.log(headersRequest.get('Authorization'))
   console.log(headerListb.get('Authorization'))
+  console.log(theme)
+  console.log(cookies().get('themeLight'))
 
   return new Response('<h1>Data users</h1>', {
     headers: {
-      'Content-Type': 'text/html'
+      'Content-Type': 'text/html',
+      'Set-Cookie': 'theme=dark'
     }
   })
 }
